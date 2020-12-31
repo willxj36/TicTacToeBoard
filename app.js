f3fc65782659db2ef347a1efd737a02a65b1729a
willxj36 <willxj36@gmail.com>
2020-12-30 19:55:46 -0700

    still working on win condition JS and corrected starting on 'O' error

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ let whoseTurn = [];
 function cellClicked () {
     if(event.target.textContent !== "X" && event.target.textContent !== "O") { 
         whoseTurn.push(1);
-        if (whoseTurn.length % 2 == 0) {
+        if (whoseTurn.length % 2 == 1) {
             event.target.textContent = "X";
         } else {
             event.target.textContent = "O";
+        }
+    }
+}
+
+function checkRow() {
+    for(i = 1 ; i < 3 ; i++) {
+        for(x = i ; x < 8 ; x+=3) {
+            if(cells[x].textContent === "X" && cells[x+1].textContent === "X" && cells[x+2].textContent === "X") {
+                //placeholder for result of if true
+            }
         }
     }
 }
