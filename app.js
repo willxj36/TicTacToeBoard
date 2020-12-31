f815c5e899480cce1ddfaa360e6d40242350d045
willxj36 <willxj36@gmail.com>
2020-12-30 18:33:23 -0700

    started JS

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 let cells = document.querySelectorAll('.row > div');
+
+for (i = 0; i < cells.length ; i++) {
+    cells[i].addEventListener('click', cellClicked);
+}
+
+let whoseTurn = [];
+
+function cellClicked () {
+    if() { 
+        whoseTurn.push(1);
+        if (whoseTurn.length % 2 == 0) {
+            event.target.textContent = "X";
+        } else {
+            event.target.textContent = "O";
+        }
+    }
+}
